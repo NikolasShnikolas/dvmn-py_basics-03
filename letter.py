7d2217523fec7d91532a1a32e1dc6b524d5ab93a
NikolasShnikolas <nikolasshnikolas@gmail.com>
2024-12-29 09:04:13 +0300

    Add files via upload

---
+++ letter.py
@@ -0,0 +1,44 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+message = """\
+Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+%website% — это новая версия онлайн-курса по программированию.
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя.
+
+Как будет проходить ваше обучение на %website%?
+
+→ Попрактикуешься на реальных кейсах.
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей.
+Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят.
+
+Регистрируйся → %website%
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
+letter = """\
+From: devmanorg@yandex.ru
+To: korsunnikolay@yandex.ru
+Subject: Приглашение!
+Content-Type: text/plain; charset="UTF-8";\n\n""" + message
+website = 'https://dvmn.org/profession-ref-program/nikolasshnikolas/QZjPq/'
+friend_name = 'Amigo'
+my_name = 'Nikolas'
+
+letter = letter.replace('%website%', website)
+letter = letter.replace('%friend_name%', friend_name)
+letter = letter.replace('%my_name%', my_name)
+
+letter = letter.encode("UTF-8")
+
+login = os.getenv('LOGIN')
+password = os.getenv('PASSWORD')
+server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
+server.login(login, password)
+server.sendmail('devmanorg@yandex.ru', 'korsunnikolay@yandex.ru', letter)
+server.quit()
